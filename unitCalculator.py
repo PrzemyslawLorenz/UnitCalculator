ee155768fefcaaac84cd2b89ddbeed87253ea051
unknown <przemoleko@gmail.com>
2020-05-11 18:58:34 +0200

    Update

--- unitCalculator.py
+++ unitCalculator.py
@@ -1,5 +1,5 @@
-def gettingValues(self):
-    print("\nWhich", self.className, "unit would you like to convert?")
+def getting_values(self):
+    print("\nWhich", type(self).__name__, "unit would you like to convert?")
     for key in self.options:
         print(int(key), ":", self.options[key])
 
@@ -8,10 +8,13 @@ def gettingValues(self):
     print("To which unit? Choose number from same options")
     self.choice2 = checking(self.options)
 
-    print("Give me your", self.className, "value: ")
+    print("Give me your", type(self).__name__, "value: ")
     self.userValue = checking()
 
-    return self.choice, self.choice2, self.userValue
+    self.inDefault = self.to_default()
+
+    print(self.userValue, "in", self.options[self.choice], "is",
+          self.to_target(), "in", self.options[self.choice2])
 
 
 def checking(options=False):
@@ -31,7 +34,7 @@ def checking(options=False):
 
 class Temperature:
     def __init__(self):
-        self.className = "temperature"
+        self.choice, self.choice2, self.userValue, self.inDefault = 0, 0, 0, 0
         self.options = {
             1.: 'Celsius',
             2.: 'Fahrenheit',
@@ -42,15 +45,11 @@ class Temperature:
             7.: 'Réaumur',
             8.: 'Rømer'
         }
-        self.choice, self.choice2, self.userValue = gettingValues(self)
 
-        self.inCelsius = self.toCelsius()
+        getting_values(self)
 
-        print(self.userValue, "in", self.options[self.choice], "is",
-              self.toTarget(), "in", self.options[self.choice2])
-
-    def toCelsius(self):
-        converter = {
+    def to_default(self):
+        converter = {  # From : To Celsius
             'Celsius': self.userValue,
             'Fahrenheit': 5 / 9 * (self.userValue - 32),
             'Kelvin': self.userValue - 273.15,
@@ -63,16 +62,16 @@ class Temperature:
 
         return converter[self.options[self.choice]]
 
-    def toTarget(self):
-        converter = {
-            'Celsius': self.inCelsius,
-            'Fahrenheit': 32 + (9 / 5 * self.inCelsius),
-            'Kelvin': self.inCelsius + 273.15,
-            'Rankine': (self.inCelsius + 273.15) * 9 / 5,
-            'Delisle': (100 - self.inCelsius) * 3 / 2,
-            'Newton': self.inCelsius * 33 / 100,
-            'Réaumur': self.inCelsius * 4 / 5,
-            'Rømer': (self.inCelsius * 21 / 40) + 7.5
+    def to_target(self):
+        converter = {  # To :
+            'Celsius': self.inDefault,
+            'Fahrenheit': 32 + (9 / 5 * self.inDefault),
+            'Kelvin': self.inDefault + 273.15,
+            'Rankine': (self.inDefault + 273.15) * 9 / 5,
+            'Delisle': (100 - self.inDefault) * 3 / 2,
+            'Newton': self.inDefault * 33 / 100,
+            'Réaumur': self.inDefault * 4 / 5,
+            'Rømer': (self.inDefault * 21 / 40) + 7.5
         }
 
         return round(converter[self.options[self.choice2]], 3)
@@ -80,21 +79,16 @@ class Temperature:
 
 class Speed:
     def __init__(self):
-        self.className = "speed"
+        self.choice, self.choice2, self.userValue, self.inDefault = 0, 0, 0, 0
         self.options = {
             1.: 'Kilometers per hour',
             2.: 'Miles per hour',
             3.: 'Meters per second'
         }
 
-        self.choice, self.choice2, self.userValue = gettingValues(self)
+        getting_values(self)
 
-        self.inKmH = self.toKmH()
-
-        print(self.userValue, "in", self.options[self.choice], "is",
-              self.toTarget(), "in", self.options[self.choice2])
-
-    def toKmH(self):
+    def to_default(self):
         converter = {  # From : To Km/H
             'Kilometers per hour': self.userValue,
             'Miles per hour': self.userValue / 0.62137,
@@ -103,11 +97,11 @@ class Speed:
 
         return converter[self.options[self.choice]]
 
-    def toTarget(self):
+    def to_target(self):
         converter = {  # To :
-            'Kilometers per hour': self.inKmH,
-            'Miles per hour': self.inKmH * 0.62137,
-            'Meters per second': self.inKmH / 3.6
+            'Kilometers per hour': self.inDefault,
+            'Miles per hour': self.inDefault * 0.62137,
+            'Meters per second': self.inDefault / 3.6
         }
 
         return round(converter[self.options[self.choice2]], 2)
@@ -115,7 +109,7 @@ class Speed:
 
 class Distance:
     def __init__(self):
-        self.className = "Distance"
+        self.choice, self.choice2, self.userValue, self.inDefault = 0, 0, 0, 0
         self.options = {
             1.: 'Metre',
             2.: 'Angstrom',
@@ -128,14 +122,9 @@ class Distance:
             9.: 'Light-year'
         }
 
-        self.choice, self.choice2, self.userValue = gettingValues(self)
+        getting_values(self)
 
-        self.inMeters = self.toMeters()
-
-        print(self.userValue, "in", self.options[self.choice], "is",
-              self.toTarget(), "in", self.options[self.choice2])
-
-    def toMeters(self):
+    def to_default(self):
         converter = {  # From : To Meters
             'Metre': self.userValue,
             'Angstrom': self.userValue * (10**-10),
@@ -150,17 +139,17 @@ class Distance:
 
         return converter[self.options[self.choice]]
 
-    def toTarget(self):
+    def to_target(self):
         converter = {  # To :
-            'Metre': self.inMeters,
-            'Angstrom': self.inMeters / (10**-10),
-            'Micron': self.inMeters / (10**-6),
-            'Inch': self.inMeters / 0.0254,
-            'Foot': self.inMeters / 0.3048,
-            'Yard': self.inMeters / 0.9144,
-            'Mile (brit.)': self.inMeters / 1609.344,
-            'Mile (nautical)': self.inMeters / 1852,
-            'Light-year': self.inMeters / (9.46 * (10**15))
+            'Metre': self.inDefault,
+            'Angstrom': self.inDefault / (10**-10),
+            'Micron': self.inDefault / (10**-6),
+            'Inch': self.inDefault / 0.0254,
+            'Foot': self.inDefault / 0.3048,
+            'Yard': self.inDefault / 0.9144,
+            'Mile (brit.)': self.inDefault / 1609.344,
+            'Mile (nautical)': self.inDefault / 1852,
+            'Light-year': self.inDefault / (9.46 * (10**15))
         }
 
         return round(converter[self.options[self.choice2]], 2)
@@ -168,7 +157,7 @@ class Distance:
 
 class Volume:
     def __init__(self):
-        self.className = "Volume"
+        self.choice, self.choice2, self.userValue, self.inDefault = 0, 0, 0, 0
         self.options = {
             1.: 'Litre',
             2.: 'Cubic metre',
@@ -180,14 +169,9 @@ class Volume:
             8.: 'Cubic inch'
         }
 
-        self.choice, self.choice2, self.userValue = gettingValues(self)
+        getting_values(self)
 
-        self.inLiters = self.toLiters()
-
-        print(self.userValue, "in", self.options[self.choice], "is",
-              self.toTarget(), "in", self.options[self.choice2])
-
-    def toLiters(self):
+    def to_default(self):
         converter = {  # From : To Liters
             'Litre': self.userValue,
             'Cubic metre': self.userValue * 1000,
@@ -201,16 +185,16 @@ class Volume:
 
         return converter[self.options[self.choice]]
 
-    def toTarget(self):
+    def to_target(self):
         converter = {  # To :
-            'Litre': self.inLiters,
-            'Cubic metre': self.inLiters / 1000,
-            'Barrel': self.inLiters / 158.987294928,
-            'Cubic foot': self.inLiters / 28.316864592,
-            'Cubic decimetre': self.inLiters / 1,
-            'Gallon (US)': self.inLiters / 3.785411784,
-            'Pint (US)': self.inLiters / 0.473176473,
-            'Cubic inch': self.inLiters / 0.016387064
+            'Litre': self.inDefault,
+            'Cubic metre': self.inDefault / 1000,
+            'Barrel': self.inDefault / 158.987294928,
+            'Cubic foot': self.inDefault / 28.316864592,
+            'Cubic decimetre': self.inDefault / 1,
+            'Gallon (US)': self.inDefault / 3.785411784,
+            'Pint (US)': self.inDefault / 0.473176473,
+            'Cubic inch': self.inDefault / 0.016387064
         }
 
         return round(converter[self.options[self.choice2]], 2)
@@ -218,19 +202,19 @@ class Volume:
 
 class Area:
     def __init__(self):
-        print("Currency")
+        self.choice, self.choice2, self.userValue, self.inDefault = 0, 0, 0, 0
         pass
 
 
 class Weight:
     def __init__(self):
-        print("Currency")
+        self.choice, self.choice2, self.userValue, self.inDefault = 0, 0, 0, 0
         pass
 
 
 class Currency:
     def __init__(self):
-        print("Currency")
+        self.choice, self.choice2, self.userValue, self.inDefault = 0, 0, 0, 0
         pass
 
 
@@ -239,6 +223,8 @@ options = {
     2.: 'Speed',
     3.: 'Distance',
     4.: 'Volume',
+    5.: 'Area',
+    6.: 'Weight',
     7.: 'Currency',
 }
 
